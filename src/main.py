821bfd49ce6d106392a6fe077398671ab86d683e
redwanul-huq <rhtamim300@gmail.com>
2026-09-07 15:56:50 +0600

    Merging

--- src/main.py
+++ src/main.py
@@ -2,4 +2,9 @@ print('Redwanul Huq Tamim')
 print("Today's Date = 06 - 09 -2026 ")
 a = 2
 b = 1
+c = a / b
 print(f'Multiplication = {a * b}')
+if b == 0:
+    print("Division not possible") 
+else:
+    print(f'Division {c}')
